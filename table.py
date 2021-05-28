8fe67cab8366b24b1bce24c81f720e5269b1eb5d
HoangNguyen0309 <70375148+HoangNguyen0309@users.noreply.github.com>
2021-05-28 19:28:40 +0700

    Update table.py

--- table.py
+++ table.py
@@ -8,6 +8,7 @@ numOfColumns = df.shape[1]
 #print(numOfColumns)
 
 print(df.loc[0][1])
+print(len(df.loc[0]))
 
 f = open("output.txt", "w")
 f.write("{| class=\"wikitable\"\n")
@@ -18,5 +19,15 @@ for col in df.columns:
     elif count != 0:
         f.write(" !! " + col)
     elif count == numOfColumns:
-        f.write(" !! " + col + "\n") 
-    count = count + 1       
+        f.write(" !! " + col + "\n|-") 
+    count = count + 1  
+for i in range(numOfRows): 
+    count2 = 0     
+    for x in range(numOfColumns):
+        if count2 == 0:
+            f.write("| " + df.loc[i][count2] + " || ")
+        elif count2 != 0:
+            f.write(df.loc[i][count2] + " || ")
+        elif count2 == numOfColumns:
+            f.write(df.loc[i][count2] + "\n")    
+        count2 = count2 + 1    
