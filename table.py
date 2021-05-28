e2fe3fddc9ef754836217137fbe9e2f7d40797f8
HoangNguyen0309 <70375148+HoangNguyen0309@users.noreply.github.com>
2021-05-28 19:45:05 +0700

    Update table.py

--- table.py
+++ table.py
@@ -5,29 +5,32 @@ df = pd.read_excel (r'tableexample.xlsx')
 numOfRows = df.shape[0]
 numOfColumns = df.shape[1]
 #print(numOfRows)
-#print(numOfColumns)
+print(numOfColumns)
+if 5 == numOfColumns -1:
+    print("BRUH!")
 
-print(df.loc[0][1])
-print(len(df.loc[0]))
+#print(df.loc[0][1])
+#print(len(df.loc[0]))
 
 f = open("output.txt", "w")
 f.write("{| class=\"wikitable\"\n")
 count = 0
 for col in df.columns:
+    #print(count)
     if count == 0:
         f.write("! " + col)
-    elif count != 0:
+    elif count != 0 and count != numOfColumns - 1:
         f.write(" !! " + col)
-    elif count == numOfColumns:
+    elif count == numOfColumns - 1:
+        print("Reaches here!")
         f.write(" !! " + col + "\n|-") 
     count = count + 1  
-for i in range(numOfRows): 
-    count2 = 0     
+for i in range(numOfRows):     
     for x in range(numOfColumns):
-        if count2 == 0:
-            f.write("| " + df.loc[i][count2] + " || ")
-        elif count2 != 0:
-            f.write(df.loc[i][count2] + " || ")
-        elif count2 == numOfColumns:
-            f.write(df.loc[i][count2] + "\n")    
-        count2 = count2 + 1    
+        if x == 0:
+            f.write("| " + str(df.loc[i][x]) + " || ")
+        elif x != 0 and x != numOfColumns - 1:
+            f.write(str(df.loc[i][x]) + " || ")
+        elif x == numOfColumns - 1:
+            print("Reaches herw!")
+            f.write(str(df.loc[i][x]) + "\n|-\n")       
