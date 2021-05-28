d3b45dc546d3a6bc19c96cead4ec0e026255d722
HoangNguyen0309 <70375148+HoangNguyen0309@users.noreply.github.com>
2021-05-28 17:57:46 +0700

    Update table.py

--- table.py
+++ table.py
@@ -4,6 +4,8 @@ import openpyxl
 df = pd.read_excel (r'tableexample.xlsx') 
 numOfRows = df.shape[0]
 numOfColumns = df.shape[1]
-numOfRows = numOfRows + 1
-print(numOfRows)
-print(numOfColumns)
+#print(numOfRows)
+#print(numOfColumns)
+for col in df.columns:
+    print(col)
+#f = open("output.txt", "w")
