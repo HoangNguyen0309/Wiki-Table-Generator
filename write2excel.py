845d1d7634134a9df1f77c881d8af5d091aee17b
HoangNguyen0309 <70375148+HoangNguyen0309@users.noreply.github.com>
2021-05-29 22:42:02 +0700

    Update write2excel.py

--- write2excel.py
+++ write2excel.py
@@ -13,21 +13,28 @@ def parse(line):
         if line[x] == '!' and line[x+1] == '!':
             num_list.append(x-1)
             num_list2.append(x+2)
-        if x == len(line):
-            num_list.append(x)
+        if x == len(line) - 1:
+            num_list.append(x + 1)
 def get_headings(num1, num2, line):
     headings_list.append(line[num1: num2])
 f = open('file.txt') # Open file on read mode
 lines = f.read().splitlines() # List with stripped line-breaks
 f.close() # Close file
 
-
+encounter = False
+new_list = []
+cells = []
+count = 0
 for line in lines:
     if line[0] == '!':
         parse(line)
         for i in range(len(num_list)):
             get_headings(num_list2[i], num_list[i], line)
-
-print(num_list2)
-print(num_list)
-print(headings_list)
+    if line[0] == '|' and line[1] != '-':
+        new_list.append(line[1:])
+    elif line[0] == '|' and (line[1] == '-' or line[1] == '}'):
+        if count != 0:
+            cells.append(new_list)
+            new_list = []
+        count = count + 1
+print(cells)
