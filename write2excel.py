18bb729a57b52fc373f008e580a4587a75d68cbc
HoangNguyen0309 <70375148+HoangNguyen0309@users.noreply.github.com>
2021-05-29 19:57:10 +0700

    Update write2excel.py

--- write2excel.py
+++ write2excel.py
@@ -1,7 +1,32 @@
 import pandas as pd
 import openpyxl
+import re
 
-bruh = "[[11, 21, 31], [12, 22, 32], [31, 32, 33]], index=['one', 'two', 'three'], columns=['a', 'b', 'c']"
-df = pd.DataFrame(bruh)
+headings_list = []
+num_list = []
+num_list2 = []
 
-print(df)
+def parse(line):
+    for x in range(len(line)):
+        if line[x] == '!' and line[x+1] == '!':
+            num_list.append[x-1]
+            num_list2.append[x+2]
+        if line[x] == '!' and line[x+1] != '!':
+            num_list2.append[x+1]
+    if x == len(line):
+        num_list.append(x)
+def get_headings(num1, num2, line):
+    headings_list.append(line[num1: num2])
+f = open('file.txt') # Open file on read mode
+lines = f.read().splitlines() # List with stripped line-breaks
+f.close() # Close file
+
+
+for line in lines:
+    if line[0] == '!':
+        parse(line)
+        for i in len(num_list):
+            get_headings(num_list2[i], num_list[i], line)
+
+
+print(headings_list)
