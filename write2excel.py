84c52559db846d70c3f8dbb7b038e31c4f5bac67
HoangNguyen0309 <70375148+HoangNguyen0309@users.noreply.github.com>
2021-05-29 20:12:25 +0700

    Update write2excel.py

--- write2excel.py
+++ write2excel.py
@@ -9,10 +9,10 @@ num_list2 = []
 def parse(line):
     for x in range(len(line)):
         if line[x] == '!' and line[x+1] == '!':
-            num_list.append[x-1]
-            num_list2.append[x+2]
+            num_list.append(x-1)
+            num_list2.append(x+2)
         if line[x] == '!' and line[x+1] != '!':
-            num_list2.append[x+1]
+            num_list2.append(x+1)
     if x == len(line):
         num_list.append(x)
 def get_headings(num1, num2, line):
@@ -25,7 +25,7 @@ f.close() # Close file
 for line in lines:
     if line[0] == '!':
         parse(line)
-        for i in len(num_list):
+        for i in range(len(num_list)):
             get_headings(num_list2[i], num_list[i], line)
 
 
