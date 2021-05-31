d963a9ffb577cd9a914d83657271ce95c59067a6
HoangNguyen0309 <70375148+HoangNguyen0309@users.noreply.github.com>
2021-05-31 11:03:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import *  
 from table import write2text
+from write2excel import writetoexcel
 
 app = Flask(__name__)  
 
@@ -14,6 +15,13 @@ def success():
         f.save(f.filename)  
         write2text(f.filename)
         return render_template("success.html", filename='output.txt')
+@app.route('/success2', methods = ['POST'])  
+def success2():  
+    if request.method == 'POST':  
+        f = request.files['file']  
+        f.save(f.filename)  
+        writetoexcel(f.filename)
+        return render_template("success2.html", filename='output.xls')
 @app.route('/database_download/<filename>')
 def database_download(filename):
     return send_file(filename, as_attachment=True)
