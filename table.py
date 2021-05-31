5fb1602e2057def3f86300693b67e1f701ac29fa
HoangNguyen0309 <70375148+HoangNguyen0309@users.noreply.github.com>
2021-05-31 09:55:28 +0700

    Update table.py

--- table.py
+++ table.py
@@ -1,36 +1,37 @@
 import pandas as pd
 import openpyxl
 
-df = pd.read_excel (r'tableexample.xlsx') 
-numOfRows = df.shape[0]
-numOfColumns = df.shape[1]
-#print(numOfRows)
-print(numOfColumns)
-if 5 == numOfColumns -1:
-    print("BRUH!")
+def write2text(file_name):
+    df = pd.read_excel(file_name) 
+    numOfRows = df.shape[0]
+    numOfColumns = df.shape[1]
+    #print(numOfRows)
+    print(numOfColumns)
+    if 5 == numOfColumns -1:
+        print("BRUH!")
 
-#print(df.loc[0][1])
-#print(len(df.loc[0]))
+    #print(df.loc[0][1])
+    #print(len(df.loc[0]))
 
-f = open("output.txt", "w")
-f.write("{| class=\"wikitable\"\n")
-count = 0
-for col in df.columns:
-    #print(count)
-    if count == 0:
-        f.write("! " + col)
-    elif count != 0 and count != numOfColumns - 1:
-        f.write(" !! " + col)
-    elif count == numOfColumns - 1:
-        print("Reaches here!")
-        f.write(" !! " + col + "\n|-") 
-    count = count + 1  
-for i in range(numOfRows):     
-    for x in range(numOfColumns):
-        if x == 0:
-            f.write("| " + str(df.loc[i][x]) + " || ")
-        elif x != 0 and x != numOfColumns - 1:
-            f.write(str(df.loc[i][x]) + " || ")
-        elif x == numOfColumns - 1:
-            print("Reaches herw!")
-            f.write(str(df.loc[i][x]) + "\n|-\n")       
+    f = open("output.txt", "w")
+    f.write("{| class=\"wikitable\"\n")
+    count = 0
+    for col in df.columns:
+        #print(count)
+        if count == 0:
+            f.write("! " + col)
+        elif count != 0 and count != numOfColumns - 1:
+            f.write(" !! " + col)
+        elif count == numOfColumns - 1:
+            print("Reaches here!")
+            f.write(" !! " + col + "\n|-\n") 
+        count = count + 1  
+    for i in range(numOfRows):     
+        for x in range(numOfColumns):
+            if x == 0:
+                f.write("| " + str(df.loc[i][x]) + " || ")
+            elif x != 0 and x != numOfColumns - 1:
+                f.write(str(df.loc[i][x]) + " || ")
+            elif x == numOfColumns - 1:
+                print("Reaches herw!")
+                f.write(str(df.loc[i][x]) + "\n|-\n")       
